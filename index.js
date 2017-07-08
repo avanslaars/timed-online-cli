3f1ad3d36d333496122d0aed06d9b6d60d8ea780
Andrew Van Slaars <avanslaars@gmail.com>
2017-07-08 09:42:06 -0400

    Grab passed in command and args and spawn a child process

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
+const {drop, isEmpty, head, tail} = require('ramda')
+const {spawn} = require('child_process')
 
-console.log('Welcome to the timed-online utility');
+const input = drop(2, process.argv)
+
+// Exit if no work to be done
+if(isEmpty(input)) {
+  console.log('timed-online needs a command to run. Exiting.')
+  process.exit(0)
+}
+
+const command = head(input)
+const args = tail(input)
+
+console.log(command, args);
+
+const child = spawn(command, args, {stdio: 'inherit'})
